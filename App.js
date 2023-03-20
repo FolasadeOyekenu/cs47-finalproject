cabc0924d7be10bcb6326ce1f5373f810fa08ef6
Folasade Oyekenu <foyekenu@DN0a1f64bc.SUNet>
2023-03-19 23:36:35 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Folasade Oyekenu <foyekenu@DN0a1f64bc.SUNet>
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   App.js
    	modified:   app.json
    	modified:   babel.config.js
    	modified:   package-lock.json
    	modified:   package.json

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './components/screens/HomeScreen';
+import Pin from './components/MapComponents/Pin'
+import DirectionsScreen from './components/screens/DirectionsScreen'
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Create a New Pin" component={Pin} />
+        <Drawer.Screen name="Navigate Around" component={DirectionsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
